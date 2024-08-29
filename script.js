8c197ed96ec1a55cbc0dd30de41763548bdde977
James <84340802+zhihaohong52@users.noreply.github.com>
2024-08-29 22:38:20 +0800

    Implement play round logic

--- script.js
+++ script.js
@@ -1,19 +1,44 @@
+var humanScore = 0;
+var computerScore = 0;
+
 function getComputerChoice() {
     var randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
-            return 'rock';
+            return 'Rock';
         case 1:
-            return 'paper';
+            return 'Paper';
         case 2:
-            return 'scissors';
+            return 'Scissors';
     }
 }
 
 function getHumanChoice() {
     var humanChoice = prompt('Please choose rock, paper, or scissors.');
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+
     return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    // Check for a tie
+    if (humanChoice === computerChoice) {
+        console.log('It\'s a tie!');
+    }
+    else if ((humanChoice === 'Rock' && computerChoice === 'Scissors') || (humanChoice === 'Paper' && computerChoice === 'Rock') || (humanChoice === 'Scissors' && computerChoice === 'Paper')) {
+        console.log('You win! ' + humanChoice + ' beats ' + computerChoice);
+        humanScore++;
+    }
+    else {
+        console.log('You lose! ' + computerChoice + ' beats ' + humanChoice);
+        computerScore++;
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
+console.log('Human Score: ' + humanScore);
+console.log('Computer Score: ' + computerScore);
